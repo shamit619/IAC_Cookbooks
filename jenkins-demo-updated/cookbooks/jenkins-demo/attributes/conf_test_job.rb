2fafc3061195cabcba63a165eb974f32072be186
shamit619 <shamit619@gmail.com>
2019-01-28 15:03:08 +0530

    Update conf_test_job.rb

--- jenkins-demo-updated/cookbooks/jenkins-demo/attributes/conf_test_job.rb
+++ jenkins-demo-updated/cookbooks/jenkins-demo/attributes/conf_test_job.rb
@@ -7,7 +7,7 @@
 #
 
 # slack notification
-default['jenkins_demo']['slack_teamdomain'] = 'DevOps'
-default['jenkins_demo']['slack_authtoken'] = 'xoxp-533436677280-535594162710-535639209158-4446b65068fa5be712ce48b3960a9045'
-default['jenkins_demo']['slack_buildserverurl'] = 'devops-a7j3270.slack.com'
-default['jenkins_demo']['slack_room'] = 'iac'
+default['jenkins_demo']['slack_teamdomain'] = ''
+default['jenkins_demo']['slack_authtoken'] = 'oHd60BtoSwt5cBuMyjyhp2ab'
+default['jenkins_demo']['slack_buildserverurl'] = 'https://devops-a7j3270.slack.com/services/hooks/jenkins-ci/'
+default['jenkins_demo']['slack_room'] = '#iac'
