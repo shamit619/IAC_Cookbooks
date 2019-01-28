e6a61c81772b361d510fd49a6440f6faa1307247
shamit619 <shamit619@gmail.com>
2019-01-29 03:14:56 +0530

    Update Jenkinsfile.groovy

--- jenkins-demo-updated/cookbooks/jenkins-demo/templates/default/JenkinsFileExample1/Jenkinsfile.groovy
+++ jenkins-demo-updated/cookbooks/jenkins-demo/templates/default/JenkinsFileExample1/Jenkinsfile.groovy
@@ -2,16 +2,19 @@
 
 node('master') {
     try {
-        stage('build') {
-            sh "echo dummy build action"
+        stage('clone') {
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/shamit619/Application.git']]])
+        }
+        stage('build and package') {
+            sh "mvn clean package"
         }
 
-        stage('test') {
-            sh "echo dummy test action"
+        stage('docker build') {
+            sh "docker build -t application:latest"
         }
 
         stage('deploy') {
-            sh "echo dummy deploy action"
+            sh "docker run -p 49160:8080 -d application:latest"
         }
     } catch(error) {
         throw error
